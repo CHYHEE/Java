3694300510b51728d2c94ee24b45a113a2684558
CHYHEE <wnn2011@naver.com>
2024-10-24 22:57:54 +0900

    10-24_pm

---
+++ src/operator/ex/OperationEx3.java
@@ -0,0 +1,10 @@
+package operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 70;
+        boolean score2 = 80 <= score && score <= 100;
+
+        System.out.println(score2);
+    }
+}
