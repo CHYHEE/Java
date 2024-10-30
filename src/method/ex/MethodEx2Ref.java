d141ac4c7814363825f7024ac0af049436ccaa94
CHYHEE <wnn2011@naver.com>
2024-10-30 11:14:57 +0900

    10-30_메서드 문제풀이1

---
+++ src/method/ex/MethodEx2Ref.java
@@ -0,0 +1,15 @@
+package method.ex;
+//반복 출력 리펙토링
+public class MethodEx2Ref {
+    public static void main(String[] args) {
+        printMessage("hello", 3);
+        printMessage("java", 5);
+        printMessage("hi", 7);
+    }
+
+    public static void printMessage(String message, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(message);
+        }
+    }
+}
