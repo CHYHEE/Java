b69157ffa8e5adb5fddf4edd926c2b599b142d2a
CHYHEE <wnn2011@naver.com>
2025-01-03 13:45:21 +0900

    1/3-클래스와 인터페이스

---
+++ src/poly/ex6/Bird.java
@@ -0,0 +1,16 @@
+package poly.ex6;
+
+import poly.ex2.Animal;
+
+public class Bird extends AbstractAnimal implements Fly {
+
+    @Override
+    public void sound() {
+        System.out.println("짹짹");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("새 날기");
+    }
+}
